32022a57ecd30c6457526cf781ac48bd3a172ce4
YkeLit <keyang117@outlook.com>
2023-04-19 10:29:40 +0800

    fix: 修改打包
    添加 package 文件

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM ubuntu:latest
 
-COPY ./install.sh /usr/local
-COPY ./vimrc /root/.vimrc
-
 WORKDIR /root
 
+COPY ./vimrc-init /root/.vimrc
+
+COPY ./install.sh /usr/local
 RUN chmod +x /usr/local/install.sh && /usr/local/install.sh
+
+COPY ./vimrc /root/.vimrc
+RUN cd /root/.vim/plugged/YouCompleteMe &&python3 install.py --go-completer --force-sudo
+RUN apt-get clean && apt purge -y build-essential cmake python3-dev
 
 CMD bash
--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ tar xzvf go1.20.3.linux-amd64.tar.gz -C /usr/local/
 
 echo "export PATH=$PATH:/usr/local/go/bin" >> /root/.bashrc
 echo export LC_ALL="C.UTF-8" >> /root/.bashrc
+echo "export PS1='\[\033[01;35m\][\[\033[01;32m\]`echo $STY | cut -d . -f2`\h\[\033[01;31m\] \u \w\[\033[31m\]\[\033[01;35m\]]\[\033[01;36m\]\$\[\033[00m\] '" >> /root/.bashrc
+ln -sf /usr/share/zoneinfo/Asia/Shanghai    /etc/localtime
+echo 'Asia/Shanghai' >/etc/timezone
 mkdir -p $HOME/go/{bin,pkg,src}
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -14,4 +17,3 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 vim -c PlugInstall -c q -c q
 vim -c GoInstallBinaries -c q -c q
 
-cd /root/.vim/plugged/YouCompleteMe &&python3 install.py --go-completer --force-sudo
