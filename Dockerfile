2b3cf78a9cb7390af5352af851e07ef33043211d
YkeLit <keyang117@outlook.com>
2023-04-19 10:54:56 +0800

    fix: vimrc 文件注释错误
    添加 golang 环境变量
    修改token

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
 FROM ubuntu:latest
 
-WORKDIR /root
-
 COPY ./vimrc-init /root/.vimrc
 
 COPY ./install.sh /usr/local
 RUN chmod +x /usr/local/install.sh && /usr/local/install.sh
 
+ENV PATH /usr/local/go/bin:$PATH
+RUN vim -c GoInstallBinaries -c q -c q && cd /root/.vim/plugged/YouCompleteMe &&python3 install.py --go-completer --force-sudo
+
 COPY ./vimrc /root/.vimrc
-RUN cd /root/.vim/plugged/YouCompleteMe &&python3 install.py --go-completer --force-sudo
 RUN apt-get clean && apt purge -y build-essential cmake python3-dev
 
-CMD bash
+ENV GOPATH /root/go
+ENV PATH $GOPATH/bin:$PATH
+RUN chmod -R 1777 "$GOPATH"
+WORKDIR $GOPATH
--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/env bash
 
-apt update && apt install wget curl git vim  build-essential cmake python3-dev -y
+apt-get update && apt-get install wget curl git vim  build-essential cmake python3-dev -y &>> /dev/null
 
-wget https://dl.google.com/go/go1.20.3.linux-amd64.tar.gz
-tar xzvf go1.20.3.linux-amd64.tar.gz -C /usr/local/
+wget https://dl.google.com/go/go1.20.3.linux-amd64.tar.gz &>> /dev/null &&\
+	tar xzvf go1.20.3.linux-amd64.tar.gz -C /usr/local/ >>/dev/null &&\
+	rm -f go1.20.3.linux-amd64.tar.gz
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> /root/.bashrc
-echo export LC_ALL="C.UTF-8" >> /root/.bashrc
-echo "export PS1='\[\033[01;35m\][\[\033[01;32m\]`echo $STY | cut -d . -f2`\h\[\033[01;31m\] \u \w\[\033[31m\]\[\033[01;35m\]]\[\033[01;36m\]\$\[\033[00m\] '" >> /root/.bashrc
-ln -sf /usr/share/zoneinfo/Asia/Shanghai    /etc/localtime
+echo "export PATH=$PATH:/usr/local/go/bin" >> /root/.bashrc &&\
+	echo export LC_ALL="C.UTF-8" >> /root/.bashrc &&\
+	export PS1='\n\e[1;37m[\e[m\e[1;32m\h\e[m\e[1;33m@\e[m\e[1;35m\u\e[m \e[4m\w\e[m\e[1;37m] \[\e[1;33mWORK\e \e[m\e[1;36m\e[m\n\$'
+
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo 'Asia/Shanghai' >/etc/timezone
 mkdir -p $HOME/go/{bin,pkg,src}
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim -c PlugInstall -c q -c q
-vim -c GoInstallBinaries -c q -c q
 
