776f307248e02380e5cd8b7876616b61a8167199
YkeLit <48484963+YkeLit@users.noreply.github.com>
2023-04-20 21:30:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,6 @@ wget https://dl.google.com/go/go1.20.3.linux-amd64.tar.gz &>> /dev/null &&\
 	tar xzvf go1.20.3.linux-amd64.tar.gz -C /usr/local/ >>/dev/null &&\
 	rm -f go1.20.3.linux-amd64.tar.gz
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> /root/.bashrc &&\
-	echo export LC_ALL="C.UTF-8" >> /root/.bashrc &&\
-	echo "export PS1='\n\e[1;37m[\e[m\e[1;32m\h\e[m\e[1;33m@\e[m\e[1;35m\u\e[m \e[4m\w\e[m\e[1;37m] \[\e[1;33mWORK\e \e[m\e[1;36m\e[m\n\$'" >> /root/.bashrc
-
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo 'Asia/Shanghai' >/etc/timezone
 mkdir -p $HOME/go/{bin,pkg,src}
