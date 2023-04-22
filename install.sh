4fb014c6279fc9f8d42f2d05f7f05991d5a1cb92
YkeLit <48484963+YkeLit@users.noreply.github.com>
2023-04-22 09:07:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-apt-get update && apt-get install wget curl git vim bash-completion tzdata -y &>> /dev/null && \
+apt-get update && apt-get install wget curl git vim bash-completion tzdata exuberant-ctags -y &>> /dev/null && \
 	apt-get autoremove -y && \
 	rm -rf /var/lib/apt/lists/* && \
 	ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime && \
@@ -10,6 +10,8 @@ apt-get update && apt-get install wget curl git vim bash-completion tzdata -y &>
 wget https://dl.google.com/go/go1.20.3.linux-amd64.tar.gz &>> /dev/null &&\
 	tar xzvf go1.20.3.linux-amd64.tar.gz -C /usr/local/ >>/dev/null &&\
 	rm -f go1.20.3.linux-amd64.tar.gz
+
+go get -u github.com/jstemmer/gotags
 
 mkdir -p $HOME/go/{bin,pkg,src}
 
