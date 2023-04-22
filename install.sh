92462adc349bc93337f5fbc65d8c49f042d30cc1
YkeLit <48484963+YkeLit@users.noreply.github.com>
2023-04-22 09:40:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,6 @@ wget https://dl.google.com/go/go1.20.3.linux-amd64.tar.gz &>> /dev/null &&\
 	tar xzvf go1.20.3.linux-amd64.tar.gz -C /usr/local/ >>/dev/null &&\
 	rm -f go1.20.3.linux-amd64.tar.gz
 
-go get -u github.com/jstemmer/gotags
-
 mkdir -p $HOME/go/{bin,pkg,src}
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
